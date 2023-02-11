5d39b906943a22574d989f9b579ae1875785c7aa
Auralius Manurung <auralius@users.noreply.github.com>
2023-02-11 20:28:55 +0700

    Octave compatible

--- visualize.m
+++ visualize.m
@@ -6,14 +6,14 @@ h_fig = figure;
 hold on ;
 
 htext = text(0,0, '');
-htext.FontWeight = 'bold';
+set(htext, "FontWeight", 'bold');
 
 h_basin = plot(0,0,'r', 'LineWidth', 3);
 h_truck = plot(0,0,'k', 'LineWidth', 3);
 h_water_surface = area(0,0, 'FaceColor', 'b');
 axis equal
-ylim([-0.5 -0.5+L]);
-
+ylim([-0.5 1.5]);
+xlim([0 max(d_T_array)+6])
 x_array = (0:dx:L)'; 
 
 for k =1 : length(h_array)
@@ -25,8 +25,8 @@ for k =1 : length(h_array)
     % Create a GIF file
     if (mod(k-1, every_nth_frame) == 0)
         write2gif(h_fig, k, fn);
-        htext.Position = [d_W_array(k)+2 -0.3];
-        htext.String = ['Time = ', num2str((k-1)*dt),' s'] ;
+        set(htext, "Position", [d_W_array(k)+2 -0.3]);
+        set(htext, "String", ['Time = ', num2str((k-1)*dt),' s']) ;
     end
 end
 
